f2cce7a414ae69ece502a8569c697a2f10e09563
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2023-05-05 20:48:17 +0200

    update site

--- generate_index.R
+++ generate_index.R
@@ -1,19 +1,8 @@
 library(quarto)
 library(deeptime)
-data("stages")
-min <- 0
-max <- 540
-by <- 10
-max_ma <- seq(from = min + by, to = max + by, by = by)
-min_ma <- seq(from = min, to = max, by = by)
-mid_ma <- (max_ma + min_ma) / 2
-pal <- colorRampPalette(stages$color, space = "Lab")
-cols <- pal(length(max_ma))
-df <- data.frame(max_ma, mid_ma, min_ma, cols)
 
+data("stages")
 df <- stages
-df$cols <- df$color
-df$max_ma <- df$max_age
 
 file.create("index.qmd")
 cat('---
@@ -25,23 +14,50 @@ format:
     controls: true
     transition: slide
     theme: [default, style.scss]
-    parallax-background-image: gradient.png
     parallax-background-size: "28px 6000px"
     self-contained: true
 ---
     ', file = "index.qmd", sep = "\n")
 
-cat(paste0('# Deep Time {background-color="white"}\n\n'), file = "index.qmd", append = TRUE)
+cat(paste0('# Origins {background-color="white"}\n\n'), file = "index.qmd", append = TRUE)
 cat(paste0('Lewis A. Jones & Miranta Kouvari\n\n'), file = "index.qmd", append = TRUE)
 
 
 for (i in 1:nrow(df)) {
-   cat(paste0('## ', df$name[i], ' {background-color="',
-             df$cols[i], '"}','\n'),
-      file = "index.qmd", append = TRUE)
-   cat(paste0('\n'), file = "index.qmd", append = TRUE)
-   cat(paste0(df$min_age[i], "--", df$max_age[i], " Ma\n\n"), file = "index.qmd", append = TRUE)
+  # Create slide
+  cat(paste0('## {background-color="',df$color[i], '"}\n\n'), file = "index.qmd", append = TRUE)
+  # Set up grid
+  cat('::: {.grid}', file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Create 3 x 2 grid
+  # Add name column
+  cat('::: {.g-col-4}', file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Add emphasis
+  cat(paste0('::: {.emphasized}'), file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Add name
+  cat(paste0(df$name[i]), file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Close emphasis
+  cat(paste0(':::'), file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Add age
+  cat(paste0(df$min_age[i], "--", df$max_age[i], " Ma\n\n"), file = "index.qmd", append = TRUE)
+  # Close name column
+  cat(':::', file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
+  # Close grid
+  cat(':::', file = "index.qmd", append = TRUE)
+  # Add space
+  cat(paste0('\n\n'), file = "index.qmd", append = TRUE)
 }
 
-quarto_render(input = "index.qmd")
-quarto_preview(file = "index.qmd")
+#quarto_render(input = "index.qmd")
+#quarto_preview(file = "index.qmd")
